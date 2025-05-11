3f5446825404b595c7c454f04d0fd04c7cc83ec2
Mitron57 <job.zakharyan@gmail.com>
2025-05-11 17:29:21 +0400

    refactor: fix env variable names in error messages

--- config/config.go
+++ config/config.go
@@ -14,11 +14,11 @@ func NewConfig() (*Config, error) {
 	var config Config
 	config.Host = os.Getenv("SUBSCRIBER_HOST")
 	if config.Host == "" {
-		return nil, errors.New("HOST environment variable not set")
+		return nil, errors.New("SUBSCRIBER_HOST environment variable not set")
 	}
 	config.Port = os.Getenv("SUBSCRIBER_PORT")
 	if config.Port == "" {
-		return nil, errors.New("PORT environment variable not set")
+		return nil, errors.New("SUBSCRIBER_PORT environment variable not set")
 	}
 	return &config, nil
 }
